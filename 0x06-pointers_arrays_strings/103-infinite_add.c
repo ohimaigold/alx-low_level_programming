1ef66dc2748f322632866aadbb8abf37b3100506
Ogedegbe O Eric <ohimaigold@gmail.com>
2022-12-22 04:04:23 -0800

    11. It is the addition of strangeness to beauty that constitutes the romantic character in art

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,86 +1,49 @@
 #include "main.h"
 
 /**
- * _strlen - returns the length of the string
- * @s: the string
+ * infinite_add - adds two numbers.
+ * @n1: First number
+ * @n2: Second number
+ * @r: result buffer
+ * @size_r: result buffer size
  *
- * Return: the length of the s string
+ * Return: 0 if the result can not be stored in r,
+ * otherwise a pointer to the result
 */
-
-int _strlen(char *s)
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i = 0;
+	int i, j, k, l, m, n;
 
-	while (s[i])
-		i++;
-	return (i);
-}
-
-/**
- * rev_n_string - reverses a string until the n th term
- * @s: the string to be reversed
- * @n: the n th term index
-*/
-
-void rev_n_string(char *s, int n)
-{
-	int i = 0;
-	int j = n - 1, max = j / 2;
-	char tmp;
-
-	while (j > max)
-	{
-		tmp = s[i];
-		s[i++] = s[j];
-		s[j--] = tmp;
-	}
-}
-
-/**
- * infinite_add - adds two numbers
- * @n1: the first number
- * @n2: the second number
- * @r: the buffer to store the result
- * @size_r: the buffer size
- *
- * Return: pointer to the result if buffor is not overflowed, else 0
-*/
-
-char	*infinite_add(char *n1, char *n2, char *r, int size_r)
-{
-	int l1 = _strlen(n1), l2 = _strlen(n2);
-	int ret = 0, j = 0, k = 0, tmp, a, b;
-
-	if (l1 >= size_r || l2 >= size_r)
+	for (i = 0; n1[i]; i++)
+		;
+	for (j = 0; n2[j]; j++)
+		;
+	if (i > size_r || j > size_r)
 		return (0);
-
-	while (k < l1 || k < l2)
+	m = 0;
+	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
 	{
-		if (k >= l1)
-			a = 0;
-		else
-			a = n1[l1 - k - 1] - '0';
-
-		if (k >= l2)
-			b = 0;
-		else
-			b = n2[l2 - k - 1] - '0';
-
-		tmp = a + b + ret;
-		ret = (tmp >= 10) ? 1 : 0;
-		r[k++] = (tmp % 10) + '0';
+		n = m;
+		if (i >= 0)
+			n += n1[i] - '0';
+		if (j >= 0)
+			n += n2[j] - '0';
+		if (i < 0 && j < 0 && n == 0)
+		{
+			break;
+		}
+		m = n / 10;
+		r[k] = n % 10 + '0';
+	}
+	r[k] = '\0';
+	if (i >= 0 || j >= 0 || m)
+		return (0);
+	for (k -= 1, l = 0; l < k; k--, l++)
+	{
+		m = r[k];
+		r[k] = r[l];
+		r[l] = m;
 	}
 
-	if (ret == 1)
-	{
-		if (k < size_r - 1)
-			r[k++] = '1';
-		else
-			return (0);
-	}
-	j = k;
-	while (j <= size_r)
-		r[j++] = '\0';
-	rev_n_string(r, k);
 	return (r);
 }
