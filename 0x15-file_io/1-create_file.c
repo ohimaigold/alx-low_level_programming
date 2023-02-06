81e60e9fe9e8790c389c85c85c1187117c4eb530
Ogedegbe O Eric <ohimaigold@gmail.com>
2023-02-05 22:54:47 -0800

    1. Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,34 +1,33 @@
 #include "main.h"
 
 /**
- * append_text_to_file - a funcion that appends text at the end
- *                      of a file
+ * create_file - a function that creates a file
  *
- * @filename: file to add data to
- * @text_content: text content to add to file
+ * @filename: name of file to create
+ * @text_content: string to write to file
  *
- * Return: 1 on success, -1 on failure
+ * Return: 1 on success OR -1 on faliure
 */
-int append_text_to_file(const char *filename, char *text_content)
+int create_file(const char *filename, char *text_content)
 {
-	int file, app_status, words = 0;
+	int file, write_status, words = 0;
 
-	if (filename == NULL) /*check if file is present*/
+	if (filename == NULL) /*check if filename is present*/
 		return (-1);
 
-	/*open file, with append option with write rights*/
-	file = open(filename, O_APPEND | O_WRONLY);
-	if (file == -1) /*check if file is present*/
+	/*open file by creating it and if it exists write but truncate to 0*/
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (file == -1) /*check if file creation was a success*/
 		return (-1);
 
-	if (text_content) /*append content to file if its not NULL*/
+	if (text_content) /*write content to file if its not NULL*/
 	{
 		while (text_content[words] != '\0') /*find number of words*/
 			words++;
 
-		/*append to file*/
-		app_status = write(file, text_content, words);
-		if (app_status == -1) /*check if append was a success*/
+		/*write to file*/
+		write_status = write(file, text_content, words);
+		if (write_status == -1) /*check if write was a success*/
 			return (-1);
 	}
 
